2d2bd16a2104fb96c10b560d34384afedbf56a57
Divij <ndivij2004@gmail.com>
2023-11-12 15:38:24 +0530

    new commit

---
+++ frontend/src/components/common/Footer.js
@@ -0,0 +1,27 @@
+function Footer(){
+    return(
+        <footer>
+            <div class="container-fluid bg-dark text-white-50 footer mt-0 pt-5 wow fadeIn" data-wow-delay="0.1s">
+                <div class="container py-5">
+                    <div class="row g-5">
+                        <div class="col-lg-3 col-md-6">
+                            <h1 class="fw-bold text-primary mb-4">AI Charity<span class="text-white">DAO</span></h1>
+                            <div class="d-flex pt-2">
+                                <a class="btn btn-square me-1" href="https://www.asroyfouncation.com"><i class="fab fa-github"></i></a>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="container-fluid copyright">
+                    <div class="container">
+                    </div>
+                </div>
+            </div>
+            <a href="https://www.asroyfouncation.com" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+        </footer>
+    )
+}
+export default Footer;
