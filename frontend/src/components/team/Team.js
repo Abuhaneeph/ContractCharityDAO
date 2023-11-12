6e2dc56519fd9a3f36b9c44c4a7d8468cb37974a
Divij <ndivij2004@gmail.com>
2023-11-12 15:36:37 +0530

    new commit

---
+++ frontend/src/components/team/Team.js
@@ -0,0 +1,17 @@
+
+function Team(){
+   
+    return(
+
+        <div className="container-xxl py-5">
+            <div className="container">
+                <div className="text-center mx-auto mb-5 wow fadeInUp mw-500" data-wow-delay="0.1s">
+                    
+                </div>
+                <div className="row g-4">
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Team;
